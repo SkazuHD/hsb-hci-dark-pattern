9f845b2cec894b644a5d38963a9f77e912833919
SkazuHD <voda0110@gmail.com>
2023-11-30 17:12:45 +0100

    some routing / useful component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {ProductDetailComponent} from "./product-detail/product-detail.component";
+import {AppComponent} from "./app.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'product/:id', component: ProductDetailComponent  },
+  { path: 'register', component: AppComponent  },
+  { path: 'login', component: AppComponent  },
+  { path: 'logout', component: AppComponent  },
+  { path: 'cart', component: AppComponent  },
+  { path: 'checkout', component: AppComponent  },
+  { path: 'shop', component: AppComponent },
+
+];
---
+++ src/app/bottom-footer/bottom-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './bottom-footer.component.html',
+  styleUrl: './bottom-footer.component.css'
+})
+export class BottomFooterComponent {
+
+}
