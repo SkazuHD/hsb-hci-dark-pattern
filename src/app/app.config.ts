b6c8514288b45491b6f04fade6e3daa40cbc2142
SkazuHD <voda0110@gmail.com>
2023-12-05 00:36:07 +0100

    Integrate fakestoreapi for testing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {ProductService} from "./product.service";
+import {provideHttpClient} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), ProductService]
+  providers: [provideRouter(routes), provideAnimations(), ProductService, provideHttpClient()]
 };
