3ff666adc0c43f9e1f141043092524d3f87e3e75
SkazuHD <voda0110@gmail.com>
2023-12-04 15:48:55 +0100

    Add Routing

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
-import {Component, Input} from '@angular/core';
+import {Component, inject, Input} from '@angular/core';
 import {Product} from "../product.service";
 import {MatButtonModule} from "@angular/material/button";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -13,5 +14,10 @@ import {MatButtonModule} from "@angular/material/button";
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  @Input({required: true}) product: Product;
+  @Input({ required: true }) product: Product;
+  private router: Router = inject(Router);
+  navigateToProductDetails(productId: number) {
+    // Use the Router service to navigate to the product details page
+    this.router.navigate(['/product', productId]);
+  }
 }
--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {Product, ProductService} from '../product.service';
+import {NgIf} from "@angular/common";
+import {filter} from "rxjs";
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-product-detail',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  productId: number;
+  product: Product; // Assuming you have a Product model
 
+  private productService: ProductService = inject(ProductService);
+  private router: Router = inject(Router);
+  private route: ActivatedRoute = inject(ActivatedRoute);
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.productId = +params['id'];
+      this.getProduct();
+    });
+  }
+
+  private getProduct() {
+    this.productService.getProductById(this.productId)
+      .pipe(
+        filter((obj): obj is Product => obj !== undefined)
+      )
+      .subscribe(
+        (product) => {
+          this.product = product;
+        },
+      );
+    if (this.product === undefined) {
+      this.router.navigate(['/product']);
+    }
+  }
 }
