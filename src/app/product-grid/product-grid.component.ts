c4fc342b8cd2759887ab8a75ad265214f9bb1dc8
SkazuHD <voda0110@gmail.com>
2023-12-07 21:17:03 +0100

    Product card styling + star system

--- src/app/product-grid/product-grid.component.ts
+++ src/app/product-grid/product-grid.component.ts
@@ -1,13 +1,14 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {ProductCardComponent} from "../product-card/product-card.component";
 import {Product, ProductService} from "../product.service";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 @Component({
   selector: 'app-product-grid',
   standalone: true,
   imports: [
     ProductCardComponent,
     NgForOf,
+    NgIf,
   ],
   templateUrl: './product-grid.component.html',
   styleUrl: './product-grid.component.css'
