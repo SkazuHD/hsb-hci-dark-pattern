94bcdcf2d279d9b8bb665f13a333551ee1f9621b
Jonas <jonas.wessling@ewetel.net>
2023-12-07 14:47:00 +0100

    Email und Password Form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,17 @@ import { Routes } from '@angular/router';
 import {ProductDetailComponent} from "./product-detail/product-detail.component";
 import {AppComponent} from "./app.component";
 import {ProductGridComponent} from "./product-grid/product-grid.component";
+import {LoginScreenComponent} from "./login-screen/login-screen.component";
 
 export const routes: Routes = [
   { path: 'product/:id', component: ProductDetailComponent  },
   { path: 'product', component: ProductGridComponent},
   { path: 'register', component: AppComponent  },
-  { path: 'login', component: AppComponent  },
+  { path: 'login', component: LoginScreenComponent  },
   { path: 'logout', component: AppComponent  },
   { path: 'cart', component: AppComponent  },
   { path: 'checkout', component: AppComponent  },
   { path: 'shop', component: AppComponent },
+  
 
 ];
---
+++ src/app/login-screen/login-screen.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule
+} from '@angular/forms';
+import {ErrorStateMatcher} from '@angular/material/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-login-screen',
+  standalone: true,
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatIconModule],
+  templateUrl: './login-screen.component.html',
+  styleUrl: './login-screen.component.css'
+
+})
+export class LoginScreenComponent {
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+
+  matcher = new MyErrorStateMatcher();
+  hide = true
+}
