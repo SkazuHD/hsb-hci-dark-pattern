f4cc3fe8e910e6feee40633735336dae226a83ef
SkazuHD <voda0110@gmail.com>
2023-12-12 15:33:43 +0100

    tailwind custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors: {
-      custom_gunmetal: "#083344",
-      custom_claret: "#8B1E3F",
-      custom_isabelline: "#EFE9E7",
-      custom_lavender: "#DAE0F2",
-      custom_darkcyan: "#379392",
+    extend: {
+      colors: {
+        custom_gunmetal: "#083344",
+        custom_claret: "#8B1E3F",
+        custom_lavender: {
+          100: "#F0F2FA",
+          200: "#DAE0F2"
+        },
+        custom_darkcyan: "#379392",
+        custom_scarlet: "#FF220C",
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
