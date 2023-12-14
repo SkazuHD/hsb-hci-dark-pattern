77ef745dca5d5f365651d040aa7193a071ccfe51
SkazuHD <voda0110@gmail.com>
2023-12-14 13:22:54 +0100

    For Deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {ProductService} from "./product.service";
 import {provideHttpClient} from "@angular/common/http";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), ProductService, provideHttpClient()]
+  providers: [provideRouter(routes), provideAnimations(), ProductService, provideHttpClient(), {provide: LocationStrategy, useClass: HashLocationStrategy}],
 };
