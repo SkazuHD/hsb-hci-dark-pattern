63dba6320c9685214a0cb448f9ec6223c8bb9797
SkazuHD <voda0110@gmail.com>
2023-12-18 16:06:11 +0100

    Loading spinner component

---
+++ src/app/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+
+@Component({
+  selector: 'app-loading-spinner',
+  standalone: true,
+  imports: [
+    NgClass,
+    MatProgressSpinnerModule
+  ],
+  templateUrl: './loading-spinner.component.html',
+  styleUrl: './loading-spinner.component.css'
+})
+export class LoadingSpinnerComponent {
+  @Input({required: true}) showLoading: boolean = false;
+}
