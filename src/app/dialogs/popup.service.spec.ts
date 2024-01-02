4b84965b3790f923188c80a1c3d716137b46602c
Jonas <jonas.wessling@ewetel.net>
2024-01-02 13:43:39 +0100

    Werbe PopUp
    Co-authored-by: RayLaxon <RayLaxon@users.noreply.github.com>

---
+++ src/app/dialogs/popup.service.spec.ts
@@ -0,0 +1,15 @@
+import { PopupService } from './popup.service';
+import {TestBed} from '@angular/core/testing';
+
+describe('PopupService', () => {
+  let service: PopupService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PopupService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
